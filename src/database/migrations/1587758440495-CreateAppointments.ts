815c8cf8f255614c50c9f702141af0ac71f4cbd1
Victor Biasibetti <victorbiasibetti7@gmail.com>
2020-04-24 18:06:17 -0300

    refector to use ORM in all methods to CR

--- src/database/migrations/1587758440495-CreateAppointments.ts
+++ src/database/migrations/1587758440495-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587758440495
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
